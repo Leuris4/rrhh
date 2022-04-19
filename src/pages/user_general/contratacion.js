390150d341979668dc6e91e15d3b4d078f39a9b8
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 <leurismorel7@gmail.com>
2022-04-19 15:21:32 -0400

    contratación

--- src/pages/user_general/contratacion.js
+++ src/pages/user_general/contratacion.js
@@ -1,36 +1,84 @@
 import React from "react";
 import '../user_general/resources/css/contratacion.css';
+import InputMask from 'react-input-mask';
 
 class Contratacion extends React.Component{
-    render(){
-        
 
+    constructor(props) {
+        super(props);
+        this.state = {hired_date: new Date()};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+        console.log(this.state);
+    }
+
+    handleSubmit(event) {
+       event.preventDefault();
+       fetch("http://localhost:4000/api/employee", {
+        method: "POST",
+        body: JSON.stringify(this.state),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json()).catch(error => console.log("error: " + error))
+    .then(response => { 
+        console.log("Success: " + JSON.stringify(response));
+        if(response.status)
+        {
+            let afp = this.state.salario * 0.0287;
+            let ars = this.state.salario * 0.0304;
+            let neto = this.state.salario - ars - afp;
+            let data = {
+                employee_id: response._id,
+                salary_brut: this.state.salario,
+                salary_net: neto,
+                ars: ars,
+                afp: afp
+            };
+            fetch("http://localhost:4000/api/payroll", {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json()).catch(error => console.log("error: " + error))
+            .then(response => console.log("payroll response: " + JSON.stringify(response)))
+        }
+    });
+       
+    }
+
+    render(){
         return <div className="container">
                     <h1>Formulario de contratación</h1>
-                    <form className="row g-3">
+                    <form onSubmit={this.handleSubmit} className="row g-3" >
                         <div className="col-12">
-                            <label for="cedula" className="form-label">Cédula</label>
-                            <input type="text" className="form-control" id="cedula" placeholder="000-0000000-0"/>
+                            <label className="form-label">Cédula</label>
+                            <InputMask name="cedula" className="form-control" id="cedula" onChange={this.handleChange} placeholder="000-0000000-0" mask="999-9999999-9"/>
                         </div>
                         <div className="col-md-6">
-                            <label for="nombre" className="form-label">Nombre</label>
-                            <input type="text" className="form-control" id="nombre" placeholder="Nombres"/>
+                            <label className="form-label">Nombre</label>
+                            <input name="name" type="text" className="form-control" id="nombre" onChange={this.handleChange} placeholder="Nombres"/>
                         </div>
                         <div className="col-md-6">
-                            <label for="apellido" className="form-label">Apellido</label>
-                            <input type="text" className="form-control" id="apellido" placeholder="Apellidos"/>
+                            <label className="form-label">Apellido</label>
+                            <input name="lastname" type="text" className="form-control" id="apellido" onChange={this.handleChange} placeholder="Apellidos"/>
                         </div>
                         <div className="col-6">
-                            <label for="departamento" className="form-label">Departamento</label>
-                            <input type="text" className="form-control" id="departamento" placeholder="Departamento"/>
+                            <label className="form-label">Departamento</label>
+                            <input name="department" type="text" className="form-control" id="departamento" onChange={this.handleChange} placeholder="Departamento"/>
                         </div>
                         <div className="col-md-4">
-                            <label for="cargo" className="form-label">Cargo</label>
-                            <input type="text" className="form-control" id="cargo" placeholder="Puesto de trabajo"/>
+                            <label className="form-label">Cargo</label>
+                            <input name="position" type="text" className="form-control" id="cargo" onChange={this.handleChange} placeholder="Puesto de trabajo"/>
                         </div>
                         <div className="col-md-2">
-                            <label for="Salario" className="form-label">Salario</label>
-                            <input type="number" className="form-control" id="Salario" placeholder="Salario"/>
+                            <label className="form-label">Salario</label>
+                            <input name="salario" type="number" className="form-control" id="Salario"  onChange={this.handleChange} placeholder="Salario"/>
                         </div>
                         <div className="col-12">
                             <button type="submit" className="btn btn-primary">Agregar</button>

49ee2a2fa9fbeedd1aa9f8318656592b298b74ee
Erickps123 <erickkpsanchez@gmail.com>
2022-04-19 11:49:55 -0400

    fixing className

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -1,14 +1,10 @@
-
 function Logout(){
     function deleteCookies() {
         document.cookie = "_id=; path=/";
         document.cookie = "_rol=; path=/";
         window.location.href="/";
     }
-    return <>{deleteCookies()}
-    
-    </>
-
+    return deleteCookies()
 }
 
 export default Logout;
